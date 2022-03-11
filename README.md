5eeab8acde886d4646139ae4751fc563b8e76796
Brooks Burns <burns.brooks@outlook.com>
2022-03-10 18:45:03 -0800

    Add README and update error handler

---
+++ README.md
@@ -0,0 +1,19 @@
+# epoch-converter
+
+Given a number of seconds, this program calculates and displays the date since the Unix epoch on January 1st, 1970. The date is displayed in MM-DD-YYYY format.
+
+## Description
+
+Since this program is intended to display a date post-Unix epoch, only non-negative values are considered valid input.
+
+## Getting Started
+
+### Executing program
+
+```
+python3 epoch_converter.py
+```
+
+## Authors
+
+Brooks Burns
--- epoch_converter.py
+++ epoch_converter.py
@@ -6,19 +6,11 @@ def epoch_converter(seconds: int) -> str:
 	"""Takes an integer representing the number of seconds since the
 	Unix epoch (1 January 1970) and returns the date in MM-DD-YYYY format
 	"""
-	try:
-		if seconds < 0:
-			raise ValueError("Input must be non-negative")
-		years_since_epoch, seconds_remaining_in_months = get_years_since_epoch(seconds)
-		year = years_since_epoch + constants.EPOCH
-		month, seconds_remaining_in_days = get_month(seconds_remaining_in_months, year)
-		day = get_day(seconds_remaining_in_days)
-		date = format_date(day, month + 1, year)  # Month converted to 1-based indexing
-	except ValueError as e:
-		print(e)
-		date = ""
-	finally:
-		return date
+	years_since_epoch, seconds_remaining_in_months = get_years_since_epoch(seconds)
+	year = years_since_epoch + constants.EPOCH
+	month, seconds_remaining_in_days = get_month(seconds_remaining_in_months, year)
+	day = get_day(seconds_remaining_in_days)
+	return format_date(day, month + 1, year)  # Month converted to 1-based indexing
 
 def get_years_since_epoch(seconds: int) -> int:
 	years = 0
@@ -83,6 +75,11 @@ def format_digits(num: int) -> str:
 	return result
 
 if __name__ == "__main__":
-	seconds = int(input("Seconds: "))
-	date = epoch_converter(seconds)
-	print(date)
+	try:
+		seconds = int(input("Seconds: "))
+		if seconds < 0:
+			raise ValueError
+		date = epoch_converter(seconds)
+		print(date)
+	except ValueError:
+		print("Input must be a non-negative integer")
